f40546a8376a68b33165ea601f9aa54b1057c8ba
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2025-12-27 12:51:01 +0000

    test adapter

--- internal/file-share/adapters/file-store/teststore.go
+++ internal/file-share/adapters/file-store/teststore.go
@@ -2,6 +2,7 @@ package filestore
 
 import (
 	"context"
+	"fmt"
 	"io"
 
 	"github.com/Miklakapi/go-file-share/internal/file-share/ports"
@@ -12,13 +13,16 @@ type TestStore struct{}
 var _ ports.FileStore = (*TestStore)(nil)
 
 func (TestStore) Save(ctx context.Context, uploadDir, name string, r io.Reader) (path string, size int64, err error) {
-	panic("TODO")
+	fmt.Printf("Saving file")
+	return name, 0, nil
 }
 
 func (TestStore) Open(ctx context.Context, path string) (io.ReadCloser, error) {
-	panic("TODO")
+	fmt.Printf("Opening file")
+	return nil, nil
 }
 
 func (TestStore) Delete(ctx context.Context, path string) error {
-	panic("TODO")
+	fmt.Printf("Deleting file")
+	return nil
 }
