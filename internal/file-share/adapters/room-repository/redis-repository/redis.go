24024d25998893ce6c1c19b1da92570718081604
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2026-01-14 06:06:11 +0000

    redis base + wipe all

--- internal/file-share/adapters/room-repository/redis-repository/redis.go
+++ internal/file-share/adapters/room-repository/redis-repository/redis.go
@@ -20,37 +20,80 @@ func New(db *redis.Client) *RedisRepo {
 }
 
 func (r *RedisRepo) Get(ctx context.Context, roomID uuid.UUID) (*domain.Room, bool, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, false, err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) List(ctx context.Context) ([]*domain.Room, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) Create(ctx context.Context, room *domain.Room) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) Delete(ctx context.Context, roomID uuid.UUID) ([]string, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) DeleteExpired(ctx context.Context, now time.Time) ([]domain.ExpiredCleanup, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) RemoveToken(ctx context.Context, roomID uuid.UUID, token string) (bool, error) {
+	if err := ctx.Err(); err != nil {
+		return false, err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) AddToken(ctx context.Context, roomID uuid.UUID, token string) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) AddFileByToken(ctx context.Context, roomID uuid.UUID, token string, file *domain.RoomFile) (bool, error) {
+	if err := ctx.Err(); err != nil {
+		return false, err
+	}
+
 	panic("TODO")
 }
 
 func (r *RedisRepo) DeleteFileByToken(ctx context.Context, roomID, fileID uuid.UUID, token string) (string, bool, error) {
+	if err := ctx.Err(); err != nil {
+		return "", false, err
+	}
+
 	panic("TODO")
+}
+
+func (r *RedisRepo) WipeAll(ctx context.Context) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	return r.db.FlushDB(ctx).Err()
 }
